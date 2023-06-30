4edee1d49d2a51f5df91ee144de8a78db6c935bf
owaischanna <124025992+owaischanna@users.noreply.github.com>
2023-06-30 14:42:28 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function openLoginForm() 
+{
+    var overlay = document.getElementById("overlay");
+    var loginForm = document.getElementById("loginForm");
+    overlay.style.display = "block";
+    loginForm.style.display = "block";
+  }
+  
+  function closeLoginForm() {
+    var overlay = document.getElementById("overlay");
+    var loginForm = document.getElementById("loginForm");
+    overlay.style.display = "none";
+    loginForm.style.display = "none";
+  }
+  
+  function submitLoginForm() {
+
+    alert("Form submitted successfully!");
+    closeLoginForm();
+    return false;
+  }
+
+
+
